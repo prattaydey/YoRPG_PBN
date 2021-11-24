8005e840bafe99103c00d7de995fbcd06301cb18
Brian Li <61071032+bli31@users.noreply.github.com>
2021-11-23 21:46:24 -0500

    updated

--- v2/Protagonist.java
+++ v2/Protagonist.java
@@ -1,34 +1,21 @@
 public class Protagonist extends Character{
-  public final double INIT_ATKRATING = 1;
-  public final int INIT_DEFENSE = 30;
-
-  public Protagonist(){
-    health = 100;
-    strength = 50;
-    defense = INIT_DEFENSE;
-    atkRating = INIT_ATKRATING;
-  }
-
   private String name;
 
   public Protagonist(String input){
+    super();
     name = input;
+    strength = 40;
   }
 
   public String getName(){
     return name;
   }
 
-  // test your luck. using consecutive special attacks will exponentially
-  // increase your attack rating, but also exponentially decrease your defense.
   public void specialize(){
-    halfDefense();
-    boostAtk();
+    // will be overwritten by subclass
   }
 
   public void normalize(){
-    resetDefense();
-    resetAtk();
+    // will be overwritten by subclass
   }
-
 }
