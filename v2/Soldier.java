2140951aa00d0217afb8a3d6d2800a50eb904e65
Brian Li <61071032+bli31@users.noreply.github.com>
2021-11-23 21:47:54 -0500

    init

---
+++ v2/Soldier.java
@@ -0,0 +1,14 @@
+public class Soldier extends Monster{
+  protected final double SOLDIER_ATKRATING = 1;
+  protected final int SOLDIER_DEFENSE = 10;
+
+  public Soldier(){
+    super();
+    atkRating = SOLDIER_ATKRATING;
+    defense = SOLDIER_DEFENSE;
+  }
+
+  public static String about(){
+    return "A soldier sent to his doom by whatever army he came from.";
+  }
+}
