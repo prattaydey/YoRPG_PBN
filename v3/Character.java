0aaf7fa31d0eaaf64323226625846bd5a7e80e89
Brian Li <61071032+bli31@users.noreply.github.com>
2021-11-24 12:33:12 -0500

    v3

--- v3/Character.java
+++ v3/Character.java
@@ -22,10 +22,6 @@ public class Character{
   public void lowerHP(int dmg){
     health -= dmg;
   }
-  
-  public int getHP(){
-    return health;
-  }
 
   public int attack(Character name){
     if (Math.random() > LUCK_THRESHOLD){ // hit!
